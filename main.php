23157ab53dcd6359d2079c5937634d825f87b043
Victor <rusuvictor30@gmail.com>
2021-01-10 23:33:49 -0500

    Login user

--- main.php
+++ main.php
@@ -16,69 +16,84 @@ define('INSTAGRAM_CLIENT_ID', '');
 /* Instagram App Client Secret */
 define('INSTAGRAM_CLIENT_SECRET', '');
 
+/* Redirect URI */
+define('REDIRECT_URI', 'https://localhost/');
+
 // require InstagramAuth Object
 require_once 'InstagramAuth.php';
 
 // create shortcode
 add_shortcode('instagram-login', 'vm_login_with_insta');
 function vm_login_with_insta(){
-    // if(!is_user_logged_in()){
-        if(!get_option('users_can_register')){
-            return('Registration is closed!');
-        }else{
-            // https://api.instagram.com/oauth/authorize?client_id=881415336022771&redirect_uri=https://localhost/&scope=user_profile,user_media&response_type=code
-            // $nonce = wp_create_nonce("vm_insta_login_nonce");
-            $redirect_uri = 'https://localhost/';
-            $data = "user_profile,user_media";
-            $fullURL = 'https://api.instagram.com/oauth/authorize?client_id='.INSTAGRAM_CLIENT_ID.'&redirect_uri='.$redirect_uri.'&scope='.$data.'&response_type=code';
-            return '
-                <a href="'.$fullURL.'">Login With Instagram</a>
-            ';
-        }
-
-        // when instagram redirected the user back
+    if(!is_user_logged_in()){
+        // when instagram redirects the user back
         if (isset($_GET['code'])) {
-            echo "dsfsfsd";
             $insta_code = $_GET['code'];
             try {
                 $instagram_C = new InstagramAuth();
                 
                 // Get the access token
-                $access_token = $instagram_C->GetToken(INSTAGRAM_CLIENT_ID, $redirect_uri, INSTAGRAM_CLIENT_SECRET, $insta_code);
+                $access_token = $instagram_C->GetToken(INSTAGRAM_CLIENT_ID, REDIRECT_URI, INSTAGRAM_CLIENT_SECRET, $insta_code);
                 // Get user information
                 $user_info = $instagram_C->GetUserProfileInformation($access_token);
-                var_dump($user_info['username']);
+                // var_dump($user_info['username']);
+
+                // check if username already registered
+                if(!username_exists($user_info['username'])){
+
+                    // generate password
+                    $bytes = openssl_random_pseudo_bytes(2);
+                    $password = md5(bin2hex($bytes));
+                    
+                    $new_user_id = wp_insert_user(array(
+                        'user_login'        => $user_info['username'],
+                        'user_pass'         => $password,
+                        // 'user_email'        => $user_email,
+                        'user_registered'   => date('Y-m-d H:i:s'),
+                        'role'              => 'subscriber'
+                    ));
+            
+                    if($new_user_id){
+                        // send an email
+                        wp_new_user_notification($new_user_id);
+            
+                        // log the new user in
+                        do_action('wp_login', $new_user_id, $user_info['username']);
+                        wp_set_current_user($new_user_id);
+                        wp_set_auth_cookie($new_user_id, true);
+            
+                        // send the newly created user to the home page
+                        wp_redirect(home_url()); exit;
+                    }
+                }else{
+                    echo 'User registered.';
+                }
+
+
+
             } catch (Exception $e) {
                 echo $e->getMessage();
                 exit;
             }
-    
+        }else{
+            // checking to see if the registration is opend
+            if(!get_option('users_can_register')){
+                return('Registration is closed!');
+            }else{
+                $data = "user_profile,user_media";
+                $fullURL = 'https://api.instagram.com/oauth/authorize?client_id='.INSTAGRAM_CLIENT_ID.'&redirect_uri='.REDIRECT_URI.'&scope='.$data.'&response_type=code';
+                return '
+                    <a href="'.$fullURL.'">Login With Instagram</a>
+                ';
+            }
         }
 
-
-
-
-    // }else{
-    //     $current_user = wp_get_current_user();
-    //     return 'Hi ' . $current_user->first_name . '! - <a href="/wp-login.php?action=logout">Log Out</a>';
-    // }
+    }else{
+        $current_user = wp_get_current_user();
+        return 'Hi ' . $current_user->first_name . '! - <a href="/wp-login.php?action=logout">Log Out</a>';
+    }
 
 }
-
-// add action 'vicode_facebook_login' to admin-ajax.php file
-// add_action('wp_ajax_vm_insta_login', 'vm_insta_login');
-// function vm_insta_login(){
-
-//     var_dump($_GET['code']);
-    
-    
-// }
-
-// // ALLOW LOGGED OUT users to access admin-ajax.php action
-// function add_ajax_actions_insta(){
-//     add_action('wp_ajax_nopriv_vm_insta_login', 'vm_insta_login');
-// }
-// add_action('admin_init', 'add_ajax_actions_insta');
 
 // redirect user to home page after log out
 function vm_redirect_after_logout(){
